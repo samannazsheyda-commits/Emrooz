ec82dee580b6234aaf1ef6d0e86b9528b37cc400
samannazsheyda-commits <samannaz.sheyda@gmail.com>
2026-09-15 16:56:46 +0330

    Add validated Shenava RNNT model profile

---
+++ hotfix_v211/RnntModelSpec.java
@@ -0,0 +1,8 @@
+package com.nameemrooz.journal.speech;
+
+/** The sherpa-onnx profile required by Shenava Koochik v1.5 RNNT. */
+public final class RnntModelSpec {
+    private RnntModelSpec() {}
+
+    public static final String MODEL_TYPE = "nemo_transducer";
+}
